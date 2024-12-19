a95135682fdebe453a32094e7dbfbb4057e7016f
utsu <admin@tel050.jp>
2024-12-19 11:33:40 +0800

    Create main.env.js

---
+++ codebase/web/modules/contrib/quicklearning_symbol/js/main.env.js
@@ -0,0 +1,6 @@
+// {104: 'MAIN_NET', 152: 'TEST_NET'} 
+export const NODE = 'https://sym-main-03.opening-line.jp:3001';
+export const epochAdjustment = 1615853185;
+export const identifier = 'mainnet';
+export const networkType = 104;
+export const generationHash = '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6';

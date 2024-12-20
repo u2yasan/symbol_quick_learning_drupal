6974cde10f02a6bbe660dc3601512b65afb6e9cf
utsu <admin@tel050.jp>
2024-12-20 22:28:58 +0800

    custom theme

---
+++ codebase/web/themes/custom/symbol_qick_learning_theme/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.symbol_qick_learning_theme = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
